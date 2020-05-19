eea6137b31f6b84ff3bc3b60fc0921f90ccd1092
Joel <j.cameron-piel@protonmail.com>
2020-05-19 12:48:01 +0800

    Added a canvas and etch-a-sketch functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+let sketch = document.querySelector('#container');
+let sketchHeight = sketch.clientHeight;
+let sketchWidth = sketch.clientWidth;
+let div;
+
+let squares = 16;
+sketchStart(squares);
+
+
+function sketchStart(squares) {
+    let totalSquares = squares * squares;
+    let squaresWidth = (sketchWidth / squares) + 'px';
+    let squaresHeight = (sketchHeight / squares) + 'px';
+
+    sketch.setAttribute('style', `display: grid; grid-template-columns: repeat(${squares}, ${squaresWidth});
+        grid-template-rows: repeat(${squares}, ${squaresHeight})`);
+
+    for (let i = 0; i < totalSquares; i++) {
+        div = document.createElement('div');
+        div.classList.add('paint');
+        sketch.appendChild(div);
+    }
+
+    let paintClass = document.querySelectorAll('.paint');
+
+    for (let i = 0; i < paintClass.length; i++) {
+        paintClass[i].addEventListener("mouseover", function (e) {
+            event.target.style.backgroundColor = "black";
+
+            eventHandler();
+        })
+    }
+
+    //sketch.addEventListener("mouseover", function(event) {
+    //    event.target.style.backgroundColor = "black";
+    //});
+}
+
